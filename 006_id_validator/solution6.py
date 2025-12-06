c0962acf480ce483cff6bcb2ff4a82222b9132d9
IgPo-da <ihor.v.popov@gmail.com>
2025-12-06 20:44:56 +0200

    The lesson 6 homework is done, file - solution6.py

---
+++ 006_id_validator/solution6.py
@@ -0,0 +1,125 @@
+pob = """001...010 = Kuressaare haigla
+011...019 = Tartu Ülikooli Naistekliinik
+021...150 = Ida-Tallinna keskhaigla, Pelgulinna sünnitusmaja (Tallinn)
+151...160 = Keila haigla
+161...220 = Rapla haigla, Loksa haigla, Hiiumaa haigla (Kärdla)
+221...270 = Ida-Viru keskhaigla (Kohtla-Järve, endine Jõhvi)
+271...370 = Maarjamõisa kliinikum (Tartu), Jõgeva haigla
+371...420 = Narva haigla
+421...470 = Pärnu haigla
+471...490 = Haapsalu haigla
+491...520 = Järvamaa haigla (Paide)
+521...570 = Rakvere haigla, Tapa haigla
+571...600 = Valga haigla
+601...650 = Viljandi haigla
+651...700 = Lõuna-Eesti haigla (Võru), Põlva haigla"""
+
+
+def enter_id_code():
+    
+    while True:
+        global id_code
+        user_input = input('Please enter your national id: ')
+        try:
+            int(user_input)
+            if len(user_input) != 11:
+                raise UserWarning()
+        except ValueError:
+            print('Only numbers alowed')
+            continue
+        except UserWarning:
+            print('Must be 11 digits long')
+            continue
+        else:
+            id_code = user_input
+            break
+
+
+def get_gender():
+    if int(id_code[0]) % 2 == 0:
+        print('Female')
+    else:
+        print('Male')
+
+
+def get_dob():
+    century = ''
+    if id_code[0] in '12':
+        century = '18'
+    elif id_code[0] in '34':
+        century = '19'
+    elif id_code[0] in '56':
+        century = '20'
+    elif id_code[0] in '78':
+        century = '21'
+    print(f'{id_code[5:7]}.{id_code[3:5]}.{century}{id_code[1:3]}')
+
+
+def validate_id():
+    chk1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+    chk2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+
+    def validator(check_weight):
+        result = 0
+        for num in range(10):
+            result += int(id_code[num]) * check_weight[num]
+        return result % 11
+    
+    # 38803160272
+    
+    if validator(chk1) == 10:
+        if validator(chk2) == int(id_code[-1]):
+            print('Code is valid 2')
+        else:
+            print('Code is invalid')
+    elif validator(chk1) == int(id_code[-1]):
+        print('Code is valid 1')
+    else:
+        print('Code is invalid')
+
+
+def menu():
+    registry = {
+        '1': get_gender,
+        '2': get_dob,
+        '3': get_rob,
+        '4': validate_id,
+        '5': enter_id_code,
+        '0': quit,
+    }
+    enter_id_code()
+    while True:
+        user_choice = input('1.Get gender\n' \
+                            '2.Get date of birth\n' \
+                            '3.Get region of birth\n' \
+                            '4.Validate id\n' \
+                            '5.Enter new id code\n' \
+                            '6.Enter something\n' \
+                            '0.Exit\n' \
+                            '--> ')
+        if user_choice in registry:
+            registry[user_choice]()
+        else:
+            print('Choice is out of range')
+
+
+def get_rob():
+    rob_number = int( id_code[-4:-1])
+    #print("place of birth number is - ", number)
+    place_found = "False"
+    for line in pob.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+        numbers_range, place = line.split("=")
+        a, b = numbers_range.split("...")
+        print(int(a), int(b))
+        if rob_number >= int(a) and rob_number <= int(b):
+            place_found = "True"
+            print("You`re born in :", place.strip())
+            break
+    if place_found == "False":
+        print("You are not born in Estonia")
+
+menu()
+
